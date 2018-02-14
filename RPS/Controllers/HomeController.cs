70736069484471883d5be5fe214909647a0a09e8
Kayla Ondracek and Nico Daunt <kayla@>
2018-02-14 10:40:52 -0800

    add post method in home controller

--- RPS/Controllers/HomeController.cs
+++ RPS/Controllers/HomeController.cs
@@ -10,5 +10,16 @@ namespace RPS.Controllers
     {
       return View();
     }
+    [HttpPost("/result")]
+    public ActionResult Result()
+    {
+      string player1 = Request.Form['player1-input'];
+      string player2 = Request.Form['player2-input'];
+
+      Game newGame = new Game(string player1, string player2)
+      newGame.SetWinner(player1, player2);
+      string winner = newGame.GetWinner();
+      return View("Index", winner);
+    }
   }
 }
