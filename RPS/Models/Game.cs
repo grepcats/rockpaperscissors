8c96bca20b0abb42a91250cbdc1bb59a0448a914
Kayla Ondracek and Nico Daunt <nico@>
2018-02-14 08:36:11 -0800

    create green test condition for GetPlayer1 test method

---
+++ RPS/Models/Game.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RPS.Models
+{
+  public class Game
+  {
+    private string _player1;
+    private string _player2;
+    private string _winner;
+
+    public Game(string player1, string player2)
+    {
+      _player1 = player1;
+      _player2 = player2;
+      _winner = "";
+    }
+
+    public string GetPlayer1()
+    {
+      return _player1;
+    }
+  }
+}
--- RPS/Program.cs
+++ RPS/Program.cs
@@ -1,7 +1,7 @@
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
 
-namespace Anagram
+namespace RPS
 {
     public class Program
     {
